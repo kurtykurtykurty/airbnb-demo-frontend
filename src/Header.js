e74cd8c9c30a17d779e90e24babfbf2f2a08d300
KurtyKurtyKurty <kurtykurtykurtykurty@gmail.com>
2017-11-08 21:50:54 +0300

    add Header.js component

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+export default function() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    )
+  }
